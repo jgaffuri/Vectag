4c356f540c79ed4e3dd9b2c3dd96e765bf35180e
jgaffuri <jgaf@protonmail.com>
2021-05-18 08:33:00 +0200

    restructure

--- src/base/canvasplus.js
+++ src/base/canvasplus.js
@@ -1,68 +1,72 @@
-//check:
-//https://github.com/kangax/fabric.js/
-//http://kineticjs.com/
-//http://www.createjs.com/Docs/EaselJS/classes/MouseEvent.html
 
-/**
- * @constructor
- * @struct
- * @param {string} div
- * @param {number=} w
- * @param {number=} h
- */
-export const CanvasPlus = function (div, w, h) {
+export class CanvasPlus {
 
-    /**@type {Object} */
-    var cvs = document.getElementById(div);
-
-    /**@type {Object} */
-    var ctx = cvs.getContext("2d");
-    if (w) ctx.canvas.width = w;
-    if (h) ctx.canvas.height = h;
-    /**@return {Object} */
-    this.getContext2D = function () { return ctx; };
+    //check:
+    //https://github.com/kangax/fabric.js/
+    //http://kineticjs.com/
+    //http://www.createjs.com/Docs/EaselJS/classes/MouseEvent.html
 
     /**
-     * @return {CanPl.CanvasPlus}
+     * @constructor
+     * @struct
+     * @param {string} canvasId
+     * @param {number=} w
+     * @param {number=} h
      */
-    this.redraw = function () {
+    constructor(canvasId, w, h) {
+        /**@type {Object} */
+        var cvs = document.getElementById(canvasId);
+
+        /**@type {Object} */
+        var ctx = cvs.getContext("2d");
+        if (w) ctx.canvas.width = w;
+        if (h) ctx.canvas.height = h;
+        /**@return {Object} */
+        this.getContext2D = function () { return ctx; };
+    }
+
+
+    /** @return {CanPl.CanvasPlus} */
+    redraw() {
         console.log("Override redraw method");
         return this;
-    };
+    }
 
     /**
      * @param {number} x
      * @param {number} y
      */
-    var pan = function (x, y) {
+    pan(x, y) {
         //TODO clear
         //TODO ctx.translate(10,10);
         //TODO updatelimits
         this.redraw();
-    };
+    }
 
     /**
      * @param {number} x
      * @param {number} y
      * @param {number} f
      */
-    var zoom = function (x, y, f) {
+    zoom(x, y, f) {
         //TODO
         //ctx.scale(2,2);
         this.redraw();
-    };
+    }
+
     /*
             $("#"+div).click(function(e) {
                 console.log(canvasClickPosition(cvs, e));
                 //TODO pan
             });
     */
+
     /**
      * @param {object} elt
      * @param {object} e
      * @return {Object.<string, number>}
      */
-    var canvasClickPosition = function (elt, e) {
+    canvasClickPosition(elt, e) {
         if (e.offsetX && e.offsetY)
             return { x: e.offsetX, y: e.offsetY };
         var tX = 0, tY = 0;
@@ -72,6 +76,6 @@ export const CanvasPlus = function (div, w, h) {
             elt = elt.offsetParent;
         } while (elt);
         return { x: e.pageX - tX, y: e.pageY - tY };
-    };
+    }
 
-};
+}
