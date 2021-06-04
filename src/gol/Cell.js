c3941a44f9f2ae52f786e1fdda054ce141da64be
jgaffuri <jgaf@protonmail.com>
2021-06-04 23:30:09 +0200

    jsdoc

---
+++ src/gol/Cell.js
@@ -0,0 +1,22 @@
+//@ts-check
+
+/**
+ * 
+ */
+export class Cell {
+
+    /**
+     * @param {number} x 
+     * @param {number} y 
+     * @param {number} nb 
+     */
+    constructor(x, y, nb=0) {
+        /** @type {number} */
+        this.x = x
+        /** @type {number} */
+        this.y = y
+        /** @type {number} */
+        this.nb = nb
+    }
+
+}
