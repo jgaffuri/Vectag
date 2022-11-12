96af38162e021b909faa7798556e9b649eebb35f
jgaffuri <jgaf@protonmail.com>
2022-11-12 17:45:55 +0100

    a

--- src/urban/RoadNetwork.js
+++ src/urban/RoadNetwork.js
@@ -11,15 +11,10 @@ export class RoadNetwork {
 
 
         //add test road
-
         //https://github.com/bjornharrtell/jsts
         //http://bjornharrtell.github.io/jsts/
         var reader = new jsts.io.WKTReader()
         var g = reader.read('LINESTRING (30 10, 10 300, 400 40)')
-        //a = a.buffer(40)
-        //var w = new jsts.io.WKTWriter()
-        //console.log(w.write(a))
-
         this.sections.push(new Road(g));
     }
 
